882e8e0833889f3be15eec1806b8d924482e1105
jskkafka <protonstar782+boot@proton.me>
2026-01-19 11:27:58 +0530

    Using Groups to oraganize the objects

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2) 
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
-        dt = clock.tick(60) / 1000
+        updatable.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
         #print(dt)
 
     
