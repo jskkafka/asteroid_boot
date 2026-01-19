b8c462eadb53d671eb45bb29f012cf4d61a69bcc
jskkafka <protonstar782+boot@proton.me>
2026-01-19 10:39:22 +0530

    Moving the player

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
         dt = clock.tick(60) / 1000
+        
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         #print(dt)
-    #screen.fill("black")
-    #pygame.display.flip()
+
     
     #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     #print(f"Screen width: {SCREEN_WIDTH}")
